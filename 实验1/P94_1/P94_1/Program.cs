c8f6db4d14fc81961e35557ef9543c2854087078
BanLi-Official <94674279+BanLi-Official@users.noreply.github.com>
2021-12-01 21:09:49 +0800

    94页代码

---
+++ 实验1/P94_1/P94_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P94_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入一串字符串：");
+            String str;
+            int number = 0;
+            str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str[i] > 64 && str[i] < 90) || (str[i] > 96 && str[i] < 123))
+                {
+                    number++;
+                }
+            }
+
+            Console.WriteLine("字符串中英文字母的个数为：{0}个", number);
+        }
+    }
+}
---
+++ 实验1/P94_2/P94_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P94_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index < 101)
+            {
+                if (index % 3 == 0)
+                {
+                    Console.WriteLine("{0}可以被3整除！",index);
+                }
+                index++;
+            }
+        }
+    }
+}
