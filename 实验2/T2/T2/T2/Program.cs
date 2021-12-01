10eb3d5b7c1827a999b233da3346b83054933aa2
BanLi-Official <94674279+BanLi-Official@users.noreply.github.com>
2021-12-01 21:12:37 +0800

    实验报告上的第2,3,题

---
+++ 实验2/T2/T2/T2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double d_a, d_b, d_c;
+            Console.WriteLine("请输入三角形三边长：");
+            Console.Write("a=");
+            d_a = Convert.ToDouble( Console.ReadLine());
+            Console.Write("b=");
+            d_b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c=");
+            d_c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("三角形面积：{0}", Surface(d_a, d_b, d_c));
+
+            Double r_a, r_b;
+            Console.WriteLine("请输入矩形边长：");
+            Console.Write("a=");
+            r_a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            r_b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("矩形面积：{0}", Surface(r_a, r_b));
+
+            Double c_a;
+            Console.WriteLine("请输入圆的半径：");
+            Console.Write("a=");
+            c_a = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("圆面积：{0}", Surface(c_a));
+
+        }
+
+        protected  static double  Surface()
+        {
+            return 1.0;
+        }
+        
+
+        public static double Surface(double a, double b, double c)
+        {
+            double S;
+            S=(1.0/4.0)*Math.Sqrt((a + b + c)*(a + b - c) * (a + c - b) * (b + c - a));
+            return S;
+        }
+
+        public static double Surface(double a, double b )
+        {
+            return a * b;
+        }
+
+        public static double Surface(double a)
+        {
+            return Math.PI * a * a;
+        }
+    }
+}
---
+++ 实验2/T3/T3/T3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            xiangliang a = new xiangliang(1, 2, 3);
+            xiangliang b = new xiangliang(3, 2, 1);
+            xiangliang c = a + b;
+            Console.WriteLine("向量a+向量b=({0},{1},{2})", c.x, c.y, c.z);
+        }
+
+
+       
+    }
+
+    class xiangliang
+    {
+        public  int x, y, z;
+        public xiangliang(int x,int y,int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public static xiangliang operator + (xiangliang a, xiangliang b)
+        {
+            return new xiangliang(a.x + b.x, a.y + b.y, a.z + b.z);
+  
+        }
+    }
+}
