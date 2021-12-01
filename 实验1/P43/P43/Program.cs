393309ef303887da7ead6c656bc90697924b0a67
BanLi-Official <94674279+BanLi-Official@users.noreply.github.com>
2021-12-01 21:07:11 +0800

    P43和82页代码

---
+++ 实验1/P43/P43/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace P43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money;
+            int month;
+            double rate_year, rate_month;
+            double pay;
+
+
+            Console.Write("贷款金额");
+            money= double.Parse(Console.ReadLine());
+            Console.Write("还款月数");
+            month = int.Parse(Console.ReadLine());
+            Console.Write("贷款利率");
+            rate_year = double.Parse(Console.ReadLine());
+
+            rate_month = rate_year / 12;
+            pay = money * rate_month * Math.Pow((1 + rate_month), month) / (Math.Pow((1 + rate_month), month) - 1);
+
+
+           
+            Console.WriteLine("每个月需还款{0}元", pay);
+
+        }
+    }
+}
---
+++ 实验1/P82/P82/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P82
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double weight = Convert.ToDouble(textBox1.Text.ToString());
+            double pay;
+            if (weight < 20)
+            {
+                pay = 0;
+                MessageBox.Show("您的托运费为" + pay + "元", "计费");
+            }
+            else if (weight <= 30)
+            {
+                pay = (weight - 20) * 30;
+                MessageBox.Show("您的托运费为" + pay + "元", "计费");
+            }
+            else if (weight <= 40)
+            {
+                pay = 300 + (weight - 30) * 40;
+                MessageBox.Show("您的托运费为" + pay + "元", "计费");
+            }
+            else if (weight <= 50)
+            {
+                pay = 300 + 400 + (weight - 40) * 50;
+                MessageBox.Show("您的托运费为" + pay + "元", "计费");
+            }
+            else 
+            {
+                MessageBox.Show("不允许携带", "计费");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
