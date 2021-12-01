3ffa58a64d2e0569e79c80b8f970e043520ad875
BanLi-Official <94674279+BanLi-Official@users.noreply.github.com>
2021-12-01 21:02:42 +0800

    期中考试代码

---
+++ code/midtest1/midtest1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace midtest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            long number;
+            Console.Write("请输入一个100000以内的值：");
+            number =Convert.ToInt32( Console.ReadLine());
+
+            long mul=1;
+            for (int index = 1; index <= number; index++)
+            {
+                mul = mul * index;
+                mul = mul % 1000;
+                
+            }
+            Console.WriteLine("{0}的阶乘结果后三位为：{1}", number,String.Format("{0:D3}", mul));
+
+
+            /*
+            Console.Write("请输入一个整数：");
+            long n = long.Parse(Console.ReadLine());
+            long i = 1;
+            long result = 1;
+            while (i <= n)
+            {
+                result = ((result % 1000) * (i % 1000)) % 1000;
+                i++;
+            }
+            Console.WriteLine("{0}!={1}", n, String.Format("{0:D3}", result));
+            Console.ReadLine();*/
+
+        }
+    }
+}
---
+++ code/midtest2/midtest2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace midtest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Fraction fenshu=new Fraction();
+            fenshu.print();
+            Fraction fenshu2 = new Fraction(-155,-25);
+            fenshu2 = +fenshu2;
+            fenshu2.print();
+
+            Fraction fenshu3 = new Fraction(-4, 0);
+            fenshu3 = +fenshu3 ;
+            fenshu3.print();
+
+            Fraction fenshu4 = new Fraction(3, -9);
+            fenshu4 = +fenshu4;
+            fenshu4.print();
+        }
+    }
+
+
+    class Fraction
+    { 
+     private
+        int molecule;//分子
+        int denominator;//分母
+        bool isError = false;
+        public Fraction()
+        {
+            molecule = 1;
+            denominator = 1;
+        }
+
+
+        public Fraction(int a,int b)
+        {
+            molecule = a;
+            denominator = b;
+            if (b == 0)
+            {
+                Console.WriteLine("出现错误！分母不能为0！构造失败！");
+                isError = true;
+            }
+        }
+
+        public void  print()
+        {
+            
+            if (this.denominator*this.molecule < 0)
+            {
+                this.molecule = -Math.Abs(this.molecule);
+                
+            }
+            if (isError == false)
+                Console.WriteLine("分数为：{0}/{1}", this.molecule, this.denominator);
+            else
+                Console.WriteLine("出现错误！分母不能为0！打印失败！");
+        }
+
+        public static Fraction operator +(Fraction x)
+        {
+            bool isfunshu = false;
+            if (x.molecule *x.denominator<0) isfunshu = true;
+            int min = Math.Min(Math.Abs(x.molecule), Math.Abs(x.denominator));
+            x.denominator = Math.Abs(x.denominator);
+            x.molecule = Math.Abs(x.molecule);
+            for (int i = 2; i <= min; i++)
+            {
+                if (x.molecule % i == 0 && x.denominator % i == 0)
+                {
+                    x.molecule /= i;
+                    x.denominator /= i;
+                    i = 1;
+                    min = Math.Min(x.molecule, x.denominator);
+                   
+                }
+               
+
+            }
+            if (isfunshu == true) x.molecule = -x.molecule;
+          
+            return x;
+        }
+
+    }
+}
