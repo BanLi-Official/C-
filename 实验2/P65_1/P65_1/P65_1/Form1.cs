d9f551ef5d239670dd1d2058b354a93e27158e7b
BanLi-Official <94674279+BanLi-Official@users.noreply.github.com>
2021-12-01 21:11:40 +0800

    P65页代码

---
+++ 实验2/P65_1/P65_1/P65_1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P65_1
+{
+    public partial class Form1 : Form
+    {
+        int math, english, chinese, chemistry;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            math = Convert.ToInt32(textBox1.Text.ToString());
+            english = Convert.ToInt32(textBox2.Text.ToString());
+            chinese= Convert.ToInt32(textBox3.Text.ToString());
+            chemistry = Convert.ToInt32(textBox4.Text.ToString());
+
+            textBox5.Text =( math + english + chinese + chemistry).ToString();
+            textBox6.Text =(Convert.ToInt32(textBox5.Text.ToString())/ 4).ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 实验2/P65_2/P65_2/P65_2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P65_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double rice, nod, quick, wlj;
+            rice = Convert.ToDouble(textBox1.Text.ToString());
+            nod = Convert.ToDouble(textBox2.Text.ToString());
+            quick = Convert.ToDouble(textBox3.Text.ToString());
+            wlj = Convert.ToDouble(textBox4.Text.ToString());
+
+            MessageBox.Show("您当前的消费金额为："+ (rice + nod + quick + wlj).ToString()+"元", "商品总价");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
